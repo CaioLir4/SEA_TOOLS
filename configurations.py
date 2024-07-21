c8b9f0490653ca9770af916f8ea3309a352cb729
CAIO <119272627+CaioLir4@users.noreply.github.com>
2024-07-21 10:46:40 -0300

    Atualizar o configurations.py

--- configurations.py
+++ configurations.py
@@ -5,7 +5,20 @@ from database.models.dash import Dash
 from database.models.indispinibilidades import Indisponibilidade
 
 
+def create_default_user():
+    default_username = 'sea'
+    default_password = 'sac@sea'
+    default_permissions = 'Home,homepage,homepage2,       homepage3,homepage4,homepage5,  homepage6,homepage7,homepage8'
 
+    try:
+       
+        if User.select().count() == 0:
+    
+            user = User.create(username=default_username, password=default_password, permissions=default_permissions)
+           
+    except IntegrityError:
+   
+        pass
 def configura_all(app):
     configure_viewes(app)
     configure_db()
@@ -16,3 +29,4 @@ def configure_viewes(app):
 def configure_db():
     db.connect()
     db.create_tables([User,Dash,Indisponibilidade])
+    create_default_user()
